62d69b2ad218a9837ea30a8b6bdff90f83832a6d
Bruno Ventura <brunoventura80@gmail.com>
2020-03-13 12:44:17 -0300

    Link para redirecionar o usuario a criacao de um alerta foi modificado

--- src/components/denunciacard/DenunciaCard.jsx
+++ src/components/denunciacard/DenunciaCard.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Button from '../button/Button';
+
+import history from '../../services/history';
+
+import './DenunciaCard.css';
 
 export default () => {
+
+    const pushCreate = () => {
+        history.push("/create");
+    }
+
     return(
         <div className="denuncia-body">
-            <Link to="/create" className="link">Clique para alertar!</Link>
+            <h4>Este é um espaço para nós, Zuppers, alertar problemas que encontramos em nosso prédio. Ajude-nos a manter o nosso ambiente o melhor possível.</h4>
+            <Button onClick={pushCreate} placeholder="Clique para alertar!" color="blue"/>
         </div>
     );
 }
