49eb6929973f714d74f05fe02231a6d1b4c9a27b
Bruno Ventura <brunoventura80@gmail.com>
2020-03-13 12:38:39 -0300

    NumberPicker foi modificado em seu estilo

--- src/components/numberpicker/NumberPicker.jsx
+++ src/components/numberpicker/NumberPicker.jsx
@@ -6,7 +6,7 @@ import './NumberPicker.css';
 export default (props) => {
     return(
         <div className="number-picker">
-            <input type="number" min="1" max="2" onChange={(e) => props.onChange(e)} className="picker" placeholder={props.placeholder} required/>    
+            <input type="number" min="0" max="1" onChange={(e) => props.onChange(e)} className="picker" placeholder={props.placeholder} required/>    
             <FontAwesomeIcon icon={props.icon} className="picker-child" />
         </div>
     );
