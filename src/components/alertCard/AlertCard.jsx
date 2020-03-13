53d1ae148fc26962fc1d8fb3b4b4593a181c62b7
Bruno Ventura <brunoventura80@gmail.com>
2020-03-13 12:45:22 -0300

    Apresentacao das informacoes de um alertCard foi modificada

--- src/components/alertCard/AlertCard.jsx
+++ src/components/alertCard/AlertCard.jsx
@@ -13,8 +13,8 @@ export default (props) => {
 
     return(
         <div className={`card-body ${props.alert.estaFeita ? 'feito' : ''}`}  id-alert={props.alert.id}>
-            <h1 className={props.alert.estaFeita ? "alerta-feito" : ""}>Andar {props.alert.andar}</h1>
-            <h1 className={props.alert.estaFeita ? "alerta-feito" : ""}>Banheiro {props.alert.generoBanheiro}</h1>
+            <h1 className={props.alert.estaFeita ? "alerta-feito" : ""}>{props.alert.andar}</h1>
+            <h1 className={props.alert.estaFeita ? "alerta-feito" : ""}>{props.alert.generoBanheiro}</h1>
             <div className="card-alert-footer">
                 <div>{props.alert.estaFeita ? <p>Feita</p> : <p>A fazer</p>}</div>
                 <Button onClick={detalhesAlert} placeholder="Ver detalhes" color="blue"/>
