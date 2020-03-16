ce17316f288b6fa03e3b35ddea881517981966aa
Bruno Ventura <brunoventura80@gmail.com>
2020-03-16 09:33:35 -0300

    Card de erro foi implementado na tela de login

--- src/components/loginform/LoginForm.jsx
+++ src/components/loginform/LoginForm.jsx
@@ -6,6 +6,7 @@ import { faUser, faUnlockAlt } from '@fortawesome/free-solid-svg-icons';
 import Textbox from '../textbox/Textbox';
 import Button from '../button/Button';
 import Loading from '../loading/Loading';
+import ErrorCard from '../errorCard/ErrorCard';
 
 import history from '../../services/history';
 import api from '../../services/api';
@@ -15,10 +16,9 @@ const Login = () => {
     
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
-    const [erro, setErro] = useState([]);
+    const [erro, setErro] = useState(null);
     const [isLogging, setIsLogging] = useState(false);
     const [isLogged, setIsLogged] = useState(false);
-    const arrayError = [];
 
     useEffect(() => {
         if(localStorage.getItem("token")){
@@ -34,32 +34,31 @@ const Login = () => {
             setIsLogged(true);
             localStorage.setItem("token", response.data.accessJwt);
         } catch (error) {
-            arrayError.push(error.response.data.error);
-            setErro(arrayError);
+            setIsLogged(false);
+            setIsLogging(false);
+            setErro(error.response.data.error);
         }
     }
     
     const handleOnChangeEmail = (e) => {
+        setErro(null);
         setEmail(e.target.value);
     }
 
     const handleOnChangePassword = (e) => {
+        setErro(null);
         setSenha(e.target.value);
     }
 
     return(
         <>
+        {erro ? <ErrorCard error={erro} /> : console.log("sem erro")}
         {isLogging ? (!isLogged ? <Loading /> : history.push("/home")) : 
             <div className="form-body">
                 <form className="form-div" onSubmit={(e) => clicarNoLogin(e)}>
-                    <Textbox icon={faUser} onChange={handleOnChangeEmail} placeholder="Email" id="email" required type="email"/>
-                    <Textbox icon={faUnlockAlt} onChange={handleOnChangePassword} placeholder="Senha" id="pswd" required type="password"/>
+                    <Textbox value={email} icon={faUser} onChange={handleOnChangeEmail} placeholder="Email" id="email" required type="email"/>
+                    <Textbox value={senha} icon={faUnlockAlt} onChange={handleOnChangePassword} placeholder="Senha" id="pswd" required type="password"/>
                     <Button placeholder="Login" color="green"/>
-                    {erro.length === 0 ? "" : 
-                        erro.map((erroName, index) =>{
-                            return <p key={index}>{erroName}</p>
-                        })
-                    }
                     <Link to="/register" className="link">Registre-se agora!</Link>
                 </form>
             </div>
