3d62b73950a56e72ec3a03fe630c89335e6ed877
Bruno Ventura <brunoventura80@gmail.com>
2020-03-16 13:30:18 -0300

    Tela de registro de usuario foi refatorada

--- src/components/registerform/RegisterForm.jsx
+++ src/components/registerform/RegisterForm.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'; 
-import Textbox from '../textbox/Textbox';
-import Button from '../button/Button';
 
 import { faUser, faUnlockAlt,faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
+import Textbox from '../textbox/Textbox';
+import Button from '../button/Button';
+import Loading from '../loading/Loading';
+import ErrorCard from '../errorCard/ErrorCard';
 
 import history from '../../services/history';
 import api from '../../services/api';
@@ -18,6 +20,8 @@ export default () => {
     const [confirmSenha, setConfirmSenha] = useState("");
     const [erro, setErro] = useState([]);
     const arrayError = [];
+    const [isLogging, setIsLogging] = useState(false);
+    const [isLogged, setIsLogged] = useState(false);
 
     const verificarCamposConfirm = () => {
         if(email !== confirmEmail){
@@ -31,70 +35,84 @@ export default () => {
 
     const clicarRegister = async (e) => {
         e.preventDefault();
+        setIsLogging(true);
 
         if(arrayError.length === 0){
-            
             verificarCamposConfirm();
             if(arrayError.length === 0){
                 try {
                     const response = await api.post("/person/PERSON", {nome, sobrenome, email, senha});
                     if(response.data){
-                        history.push("");
+                        setIsLogged(true);
                     }
                 } catch (error) {
+                    setIsLogged(false);
+                    setIsLogging(false);
                     arrayError.push(error.response.data.error);
                     setErro(arrayError);
                 }
             }else{
+                setIsLogged(false);
+                setIsLogging(false);
                 setErro(arrayError);
             }
         }else{
+            setIsLogged(false);
+            setIsLogging(false);
             setErro(arrayError);
         }
     }
     
     const handleOnChangeEmail = (e) => {
+        setErro([]);
         setEmail(e.target.value);
     }
 
     const handleOnChangeConfirmEmail = (e) => {
+        setErro([]);
         setConfirmEmail(e.target.value);
     }
 
     const handleOnChangePassword = (e) => {
+        setErro([]);
         setSenha(e.target.value);
     }
 
     const handleOnChangeConfirPassword = (e) => {
+        setErro([]);
         setConfirmSenha(e.target.value);
     }
 
     const handleOnChangeNome = (e) => {
+        setErro([]);
         setNome(e.target.value);
     }
 
     const handleOnChangeSobrenome = (e) => {
+        setErro([]);
         setSobrenome(e.target.value);
     }
 
     return(
-        <div className="form-body">
-            <form className="form-div" onSubmit={(e) => clicarRegister(e)}>
-                <Textbox icon={faUser} onChange={handleOnChangeNome} placeholder="Nome" id="nome" required type="text"/>
-                <Textbox icon={faUser} onChange={handleOnChangeSobrenome} placeholder="Sobrenome" id="sobrenome" required type="text"/>
-                <Textbox icon={faEnvelope} onChange={handleOnChangeEmail} placeholder="Email" id="email" required type="email"/>
-                <Textbox icon={faEnvelope} onChange={handleOnChangeConfirmEmail} placeholder="Confirmar email" id="confirmEmail" required type="email"/>
-                <Textbox icon={faUnlockAlt} onChange={handleOnChangePassword} placeholder="Senha" id="pswd" required type="password"/>
-                <Textbox icon={faUnlockAlt} onChange={handleOnChangeConfirPassword} placeholder="Confirmar senha" id="confirmPswd" required type="password" />
-                {erro.length === 0 ? "" : 
-                    erro.map((erroName, index) =>{
-                        return <p key={index}>{erroName}</p>
-                    })
-                }
-                <Button placeholder="Registrar" color="green"/>
+        <>
+        {erro.length != 0 ? erro.map((erroTitle, index) => {
+           return <ErrorCard key={index} error={erroTitle}/>
+        }) : ""}
+        {isLogging ? (!isLogged ? <Loading /> : history.push("")) : 
+            <div className="form-body">
+                <form className="form-div" onSubmit={(e) => clicarRegister(e)}>
+                    <Textbox value={nome} icon={faUser} onChange={handleOnChangeNome} placeholder="Nome" id="nome" required type="text"/>
+                    <Textbox value={sobrenome} icon={faUser} onChange={handleOnChangeSobrenome} placeholder="Sobrenome" id="sobrenome" required type="text"/>
+                    <Textbox value={email} icon={faEnvelope} onChange={handleOnChangeEmail} placeholder="Email" id="email" required type="email"/>
+                    <Textbox value={confirmEmail} icon={faEnvelope} onChange={handleOnChangeConfirmEmail} placeholder="Confirmar email" id="confirmEmail" required type="email"/>
+                    <Textbox value={senha} icon={faUnlockAlt} onChange={handleOnChangePassword} placeholder="Senha" id="pswd" required type="password"/>
+                    <Textbox value={confirmSenha} icon={faUnlockAlt} onChange={handleOnChangeConfirPassword} placeholder="Confirmar senha" id="confirmPswd" required type="password" />
+                    <Button placeholder="Registrar" color="green"/>
 
-                <Link to="/" className="link">Já possui uma conta? Faça o login!</Link>
-            </form>
-        </div>
+                    <Link to="/" className="link">Já possui uma conta? Faça o login!</Link>
+                </form>
+            </div>
+        }
+        </>
     );
 }
