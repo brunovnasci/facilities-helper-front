2926d7216b0f38473bfa67227e095515f165a732
Bruno Ventura <brunoventura80@gmail.com>
2020-03-16 08:52:10 -0300

    Usuario visualiza uma tela de loading enquanto a requisicao de login e feita

--- src/components/loginform/LoginForm.jsx
+++ src/components/loginform/LoginForm.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router';
 import { Link } from 'react-router-dom';
+import { faUser, faUnlockAlt } from '@fortawesome/free-solid-svg-icons';
 
 import Textbox from '../textbox/Textbox';
 import Button from '../button/Button';
-import { faUser, faUnlockAlt } from '@fortawesome/free-solid-svg-icons';
+import Loading from '../loading/Loading';
 
 import history from '../../services/history';
 import api from '../../services/api';
@@ -15,6 +16,8 @@ const Login = () => {
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
     const [erro, setErro] = useState([]);
+    const [isLogging, setIsLogging] = useState(false);
+    const [isLogged, setIsLogged] = useState(false);
     const arrayError = [];
 
     useEffect(() => {
@@ -25,10 +28,11 @@ const Login = () => {
 
     const clicarNoLogin = async (e) => {
         e.preventDefault();
+        setIsLogging(true);
         try {
             const response = await api.post('person/jwt', {email, senha});
+            setIsLogged(true);
             localStorage.setItem("token", response.data.accessJwt);
-            history.push("/home");
         } catch (error) {
             arrayError.push(error.response.data.error);
             setErro(arrayError);
@@ -44,19 +48,23 @@ const Login = () => {
     }
 
     return(
-        <div className="form-body">
-            <form className="form-div" onSubmit={(e) => clicarNoLogin(e)}>
-                <Textbox icon={faUser} onChange={handleOnChangeEmail} placeholder="Email" id="email" required type="email"/>
-                <Textbox icon={faUnlockAlt} onChange={handleOnChangePassword} placeholder="Senha" id="pswd" required type="password"/>
-                <Button placeholder="Login" color="green"/>
-                {erro.length === 0 ? "" : 
-                    erro.map((erroName, index) =>{
-                        return <p key={index}>{erroName}</p>
-                    })
-                }
-                <Link to="/register" className="link">Registre-se agora!</Link>
-            </form>
-        </div>
+        <>
+        {isLogging ? (!isLogged ? <Loading /> : history.push("/home")) : 
+            <div className="form-body">
+                <form className="form-div" onSubmit={(e) => clicarNoLogin(e)}>
+                    <Textbox icon={faUser} onChange={handleOnChangeEmail} placeholder="Email" id="email" required type="email"/>
+                    <Textbox icon={faUnlockAlt} onChange={handleOnChangePassword} placeholder="Senha" id="pswd" required type="password"/>
+                    <Button placeholder="Login" color="green"/>
+                    {erro.length === 0 ? "" : 
+                        erro.map((erroName, index) =>{
+                            return <p key={index}>{erroName}</p>
+                        })
+                    }
+                    <Link to="/register" className="link">Registre-se agora!</Link>
+                </form>
+            </div>
+        }
+        </>
     );
 }
 
